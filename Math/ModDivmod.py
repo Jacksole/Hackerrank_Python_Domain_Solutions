b5a66eec52df65f948903e65e30ad4f7dd7d2785
Le Aundre Jackson <dell.5287@gmail.com>
2019-11-04 16:00:54 -0500

    Edit Files
    added __name__== '__main___' to program

--- Math/ModDivmod.py
+++ Math/ModDivmod.py
@@ -7,9 +7,10 @@ Created   : 15 July 2016
 Problem   : https://www.hackerrank.com/challenges/python-mod-divmod/problem
 '''
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-a=int(input())
-b=int(input())
-d=divmod(a,b)
-print (d[0])
-print (d[1])
-print (d)
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    d = divmod(a, b)
+    print(d[0])
+    print(d[1])
+    print(d)
