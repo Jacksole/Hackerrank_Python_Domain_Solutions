0a23573011596c0f2eb1a8a00ae92fe7c7fc1916
Le Aundre Jackson <dell.5287@gmail.com>
2019-11-07 16:38:32 -0500

    Updated format

--- Strings/AlphabetRangoli.py
+++ Strings/AlphabetRangoli.py
@@ -6,35 +6,15 @@ Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
 Problem   : https://www.hackerrank.com/challenges/alphabet-rangoli/problem
 '''
-n = int(input().strip())
-w = (n-1) * 2 + ((n * 2) - 1)
-#upper half
-for i in range(1,n,1):
-    number_of_letter = (i*2) - 1
-    s = ''
-    letter_value = 97 + n - 1
-    for i in range(0,number_of_letter):
-        if(i != 0):
-            s += '-' 
-        s += chr(letter_value) 
-        if(i<(number_of_letter-1) / 2):
-            letter_value = letter_value - 1
-        else:
-            letter_value = letter_value + 1            
-    print(s.center(w,'-'))
-    
-    
-#bottom half
-for i in range(n,0,-1):
-    number_of_letter = (i*2) - 1
-    s = ''
-    letter_value = 97 + n - 1
-    for i in range(0,number_of_letter):
-        if(i != 0):
-            s += '-' 
-        s += chr(letter_value) 
-        if(i<(number_of_letter-1) / 2):
-            letter_value = letter_value - 1
-        else:
-            letter_value = letter_value + 1            
-    print(s.center(w,'-'))
+import string
+
+def print_rangoli(size):
+    width = 4 * size - 3
+        alpha = string.ascii_lowercase
+        for i in list(range(size))[::-1] + list(range(1, size)):
+            print('-'.join(alpha[size-1:i:-1] + alpha[i:size]).center(width, '-'))
+
+
+if __name__ == '__main__':
+    n  = int(input())
+    print_rangoli(n)
--- Strings/Capitalize.py
+++ Strings/Capitalize.py
@@ -6,10 +6,12 @@ Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
 Problem   : https://www.hackerrank.com/challenges/capitalize/problem
 '''
-s = input()
-s_ar = s.split(' ')
-final_ar = []
-space = ' '
-for w in s_ar:
-    final_ar.append(w.capitalize())
-print(space.join(final_ar))
+
+if __name__ == '__main__':
+    s = input()
+    s_ar = s.split(' ')
+    final_ar = []
+    space = ' '
+    for w in s_ar:
+        final_ar.append(w.capitalize())
+    print(space.join(final_ar))
